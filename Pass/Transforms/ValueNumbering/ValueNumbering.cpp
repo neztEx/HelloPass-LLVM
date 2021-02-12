55de3b4c8db3f9872c7f18129bbaf350863875a1
Sam Tapia <stapi010@ucr.edu>
2021-02-11 21:40:55 -0800

    working valueNumber, added sub, mult, div, fixed ordering issue

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -22,7 +22,7 @@ struct ValueNumbering : public FunctionPass {
     static char ID;
     
     map<llvm::Value *,int> valTable;
-    map<int, int> redTable;
+    map<string, int> redTable;
     int valueNumber = 0;
     string op = "";
     string repeat = "";
@@ -32,7 +32,7 @@ struct ValueNumbering : public FunctionPass {
     bool runOnFunction(Function &F) override {
 	    
         errs() << "ValueNumbering: " << F.getName() << "\n";
-        if (F.getName() != func_name) return false;
+        // if (F.getName() != func_name) return false;
 
         for (auto& basic_block : F)
         {
@@ -40,36 +40,48 @@ struct ValueNumbering : public FunctionPass {
             {
                 // errs() << inst << "\n";
                 // errs() << inst.getOperand(0) << "\n";
-                if (inst.getOpcode() != Instruction::Alloca){
-                    errs() << formatv("{0,-40}", inst);
-                    // errs() << " " << inst.getOperandList()->getUser() << "\n";
+                if (inst.getOpcode() == Instruction::Alloca){
+                    // errs() << formatv("{0,-40}", inst);
+                    continue;
                 }
 
                 if(inst.getOpcode() == Instruction::Load){
                     // errs() << "Load ";
+                    // errs() << inst.getOperand(0) << "\n";
+                    // errs() << inst.getOperand(1) << "\n";
                     if (valTable.find(inst.getOperand(0)) == valTable.end()){
                         valTable[inst.getOperand(0)] = ++valueNumber;
                     }
+                    if (valTable.find(inst.getOperandList()->getUser()) == valTable.end()){
+                        valTable[inst.getOperandList()->getUser()] = valTable[inst.getOperand(0)];
+                    }
 
-                    // errs() << "\nOperand(0) Mem: " << (*inst.getOperand(0)) <<"\n";
-                    // errs() << "\nOperand(1) Mem: " << (*inst.getOperand(1)) <<"---\n";
-                    errs() << valTable[inst.getOperand(0)] << " = " << valTable[inst.getOperand(0)] << "\n";
+                    errs() << formatv("{0,-40}", inst) << valTable[inst.getOperandList()->getUser()] << " = " << valTable[inst.getOperandList()->getUser()] << "\n";
                 }
                 if(inst.getOpcode() == Instruction::Store){
                     // errs() << "Store ";
                     // if (inst.getOperand(0)){
-                    //     errs() << inst.getOperand(0) << "\n";
-                    //     errs() << inst.getOperand(1) << "\n";
+                        // errs() << inst.getOperand(0) << "\n";
+                        // errs() << inst.getOperand(1) << "\n";
                     // }
-                    if (valTable.find(inst.getOperand(0)) != valTable.end()){
-                            valTable[inst.getOperand(1)] = valTable[inst.getOperand(0)];
-                            errs() << valTable[inst.getOperand(1)] << " = " << valTable[inst.getOperand(1)] << "\n";
+                    // if (valTable.find(inst.getOperand(0)) != valTable.end()){
+                    //     valTable[inst.getOperand(1)] = valTable[inst.getOperand(0)];
+                    //     errs() << formatv("{0,-40}", inst) << valTable[inst.getOperand(1)] << " = " << valTable[inst.getOperand(1)] << "\n";
+                    // }
+                    // else if (valTable.find(inst.getOperand(1)) == valTable.end()){
+                    //     valTable[inst.getOperand(1)] = ++valueNumber;
+                    //     errs() << formatv("{0,-40}", inst) << valTable[inst.getOperand(1)] << " = " << valTable[inst.getOperand(1)] << "\n";
+                    // }
+
+                    if (valTable.find(inst.getOperand(0)) == valTable.end()){
+                        valTable[inst.getOperand(0)] = ++valueNumber;
+                        valTable[inst.getOperand(1)] = valTable[inst.getOperand(0)];
+                        errs() << formatv("{0,-40}", inst) << valTable[inst.getOperand(0)] << " = " << valTable[inst.getOperand(1)] << "\n";
                     }
-                    else if (valTable.find(inst.getOperand(1)) == valTable.end()){
-                        valTable[inst.getOperand(1)] = ++valueNumber;
-                        errs() << valTable[inst.getOperand(1)] << " = " << valTable[inst.getOperand(1)] << "\n";
-                    }
-                    //  errs() << inst.getOperandList()->get() << "\n";
+                    valTable[inst.getOperand(1)] = valTable[inst.getOperand(0)];
+                    errs() << formatv("{0,-40}", inst) << valTable[inst.getOperand(0)] << " = " << valTable[inst.getOperand(1)] << "\n";
+
+                    // errs() << inst.getOperandList()->get() << "\n";
                     // errs() << "\nOperand(1)" << (*inst.getOperand(1)) <<"\n";
                     // errs() << "\nOperand(1) Mem: " << inst.getOperand(1) <<"---\n";
                 }
@@ -79,6 +91,12 @@ struct ValueNumbering : public FunctionPass {
                     if(inst.getOpcode() == Instruction::Add){
                         // errs() << "Add ";
                         op = "Add";
+                        if (valTable.find(inst.getOperand(0)) == valTable.end()){
+                            valTable[inst.getOperand(0)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperand(1)) == valTable.end()){
+                            valTable[inst.getOperand(1)] = ++valueNumber;
+                        }
                         if (valTable.find(inst.getOperandList()->getUser()) == valTable.end()){
                             valTable[inst.getOperandList()->getUser()] = ++valueNumber;
                         }
@@ -90,14 +108,47 @@ struct ValueNumbering : public FunctionPass {
                     if(inst.getOpcode() == Instruction::Sub){
                         // errs() << "Sub ";
                         op = "Sub";
+                        if (valTable.find(inst.getOperand(0)) == valTable.end()){
+                            valTable[inst.getOperand(0)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperand(1)) == valTable.end()){
+                            valTable[inst.getOperand(1)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperandList()->getUser()) == valTable.end()){
+                            valTable[inst.getOperandList()->getUser()] = ++valueNumber;
+                        }
+
+
                     }
                     if(inst.getOpcode() == Instruction::Mul){
                         // errs() << "Mult ";
                         op = "Mult";
+                        if (valTable.find(inst.getOperand(0)) == valTable.end()){
+                            valTable[inst.getOperand(0)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperand(1)) == valTable.end()){
+                            valTable[inst.getOperand(1)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperandList()->getUser()) == valTable.end()){
+                            valTable[inst.getOperandList()->getUser()] = ++valueNumber;
+                        }
+
+
                     }
                     if(inst.getOpcode() == Instruction::UDiv){
                         // errs() << "Div ";
                         op = "Div";
+                        if (valTable.find(inst.getOperand(0)) == valTable.end()){
+                            valTable[inst.getOperand(0)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperand(1)) == valTable.end()){
+                            valTable[inst.getOperand(1)] = ++valueNumber;
+                        }
+                        if (valTable.find(inst.getOperandList()->getUser()) == valTable.end()){
+                            valTable[inst.getOperandList()->getUser()] = ++valueNumber;
+                        }
+
+
                     }
                     
                     // see other classes, Instruction::Sub, Instruction::UDiv, Instruction::SDiv
@@ -114,20 +165,28 @@ struct ValueNumbering : public FunctionPass {
                         // errs() << dyn_cast<Instruction>(it)->getOperand(0) << "\n";
                         // (*it)->setName(to_string(valueNumber));
                         // errs() << valTable[dyn_cast<Instruction>(it)->getOperand(0)];
+                        // errs() << "PRINTING INT: " << (*it) << "\n";
+                        // if (valTable.find((*it)) == valTable.end()){
+                        //     // errs() << "HERE\n";
+                        //     valTable[(*it)] = ++valueNumber;
+                        //     errs() << formatv("{0,-40}", *(*it)) << valTable[(*it)] << " = " << valTable[(*it)] << "\n";
+                        // }
                         if (i == 0){
                             // errs() << valTable[dyn_cast<Instruction>(it)->getOperand(0)] << " " + op + " ";
-                            temp1 = valTable[dyn_cast<Instruction>(it)->getOperand(0)];
+                            temp1 = valTable[(*it)];
                         }
                         else{
                             // errs() << valTable[dyn_cast<Instruction>(it)->getOperand(0)] << "\n";
 
-                            temp2 = valTable[dyn_cast<Instruction>(it)->getOperand(0)];
-                            if (redTable.find(temp1+temp2) == redTable.end()){
-                                redTable[temp1+temp2] = valTable[inst.getOperandList()->getUser()];
+                            temp2 = valTable[(*it)];
+                            string temp = to_string(temp1) + op + to_string(temp2);
+
+                            if (redTable.find(temp) == redTable.end()){
+                                redTable[temp] = valTable[inst.getOperandList()->getUser()];
                             }
                             else{
                                 repeat = " (redundant) ";
-                                valTable[inst.getOperandList()->getUser()] = redTable[temp1+temp2];
+                                valTable[inst.getOperandList()->getUser()] = redTable[temp];
                             }
                         }
                         i++;
@@ -135,7 +194,7 @@ struct ValueNumbering : public FunctionPass {
             			//     errs() << (*it)->getName() << "\n";                      
                     }
                     //output for binary OP w/ redundant computation output
-                    errs() << valTable[inst.getOperandList()->getUser()] << " = " << temp1 << " " + op + " " << temp2 << repeat << "\n";
+                    errs() << formatv("{0,-40}", inst) << valTable[inst.getOperandList()->getUser()] << " = " << temp1 << " " + op + " " << temp2 << repeat << "\n";
                     repeat = "";
                 } // end if
             } // end for inst
